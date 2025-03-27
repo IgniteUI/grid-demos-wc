d4e741b0873942fb4e44ffa623a0fc98e689af8b
Martin Dragnev <mddragnev@gmail.com>
2025-03-27 13:43:56 +0200

    Add vite plugin to copy web.config to the dist when built

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+export default defineConfig({
+  resolve: {
+    alias: {
+      "igniteui-theming": new URL("./node_modules/igniteui-theming", import.meta.url).pathname,
+    },
+  },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: "./web.config",
+          dest: "",
+        },
+      ],
+    }),
+  ],
+});
