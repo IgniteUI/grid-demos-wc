e1dccac2bd926378271bc4abb2b6d0a6ea377e92
Martin Dragnev <mddragnev@gmail.com>
2025-04-08 14:25:55 +0300

    Add loading indicator to the iframe

--- src/views/finance/finance-view.ts
+++ src/views/finance/finance-view.ts
@@ -2,16 +2,23 @@ import { LitElement, html, unsafeCSS } from "lit";
 import { customElement } from "lit/decorators.js";
 import "../../../projects/finance-grid/src/finance-grid";
 import namedStyles from "./finance-view.scss?inline";
+import sharedStyles from "../../shared/styles.scss?inline";
 
 @customElement("finance-view")
 export default class FinanceView extends LitElement {
   constructor() {
     super();
   }
+
+  private onLoad = (event: any) => {
+    event.target.parentElement.classList.remove("loading");
+  };
   render() {
     const iframeSrc = `${import.meta.env.BASE_URL}finance`;
-    return html` <iframe src=${iframeSrc}></iframe> `;
+    return html` <div class="iframe-wrapper loading">
+      <iframe src=${iframeSrc} @load=${this.onLoad}></iframe>
+    </div>`;
   }
 
-  static styles = unsafeCSS(namedStyles);
+  static styles = [unsafeCSS(namedStyles), unsafeCSS(sharedStyles)];
 }
--- src/views/hr-portal/hr-portal-view.ts
+++ src/views/hr-portal/hr-portal-view.ts
@@ -2,17 +2,23 @@ import { LitElement, html, unsafeCSS } from "lit";
 import { customElement } from "lit/decorators.js";
 import "../../../projects/hr-portal/src/hr-portal";
 import namedStyles from "./hr-portal-view.scss?inline";
+import sharedStyles from "../../shared/styles.scss?inline";
 
 @customElement("hr-portal-view")
 export default class HrPortalView extends LitElement {
   constructor() {
     super();
   }
+  private onLoad = (event: any) => {
+    event.target.parentElement.classList.remove("loading");
+  };
   render() {
     const iframeSrc = `${import.meta.env.BASE_URL}hr-portal`;
 
-    return html` <iframe src=${iframeSrc}></iframe> `;
+    return html` <div class="iframe-wrapper loading">
+      <iframe src=${iframeSrc} @load=${this.onLoad}></iframe>
+    </div>`;
   }
 
-  static styles = unsafeCSS(namedStyles);
+  static styles = [unsafeCSS(namedStyles), unsafeCSS(sharedStyles)];
 }
