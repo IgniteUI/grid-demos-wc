2004670580e02e9c4b03486145d6ee8f6de341ab
Martin Dragnev <37667452+mddragnev@users.noreply.github.com>
2025-04-16 11:00:36 +0300

    Add main view with tabs and routing (#5)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,1 @@
 /// <reference types="vite/client" />
-declare module "process" {
-  global {
-    namespace NodeJS {
-      interface ProcessEnv {
-        NODE_ENV: "development" | "production";
-      }
-    }
-  }
-}
