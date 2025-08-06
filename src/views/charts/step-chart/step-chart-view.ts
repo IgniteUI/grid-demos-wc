d9b982a91adbb3c90c195cd399a17c72613e781f
Mariela Tihova <MTihova@infragistics.com>
2025-08-06 10:51:57 +0300

    Added Step Chart view

---
+++ src/views/charts/step-chart/step-chart-view.ts
@@ -0,0 +1,25 @@
+import { LitElement, unsafeCSS, html } from "lit";
+import { customElement } from "lit/decorators.js";
+import "../../../../projects/charts/step-chart/src/step-chart";
+import namedStyles from "./step-chart-view.scss?inline";
+// import sharedStyles from "../../shared/styles.scss?inline";
+import sharedStyles from "../../../shared/styles.scss?inline";
+
+@customElement("step-chart-view")
+export default class StepChartView extends LitElement {
+  constructor() {
+    super();
+  }
+
+  private onLoad = (event: any) => {
+    event.target.parentElement.classList.remove("loading");
+  };
+
+  render() {
+    const iframeSrc = `${import.meta.env.BASE_URL}charts/step-chart`;
+    return html` <div class="iframe-wrapper loading">
+      <iframe src=${iframeSrc} @load=${this.onLoad}></iframe>
+    </div>`;
+  }
+  static styles = [unsafeCSS(namedStyles), unsafeCSS(sharedStyles)];
+}
